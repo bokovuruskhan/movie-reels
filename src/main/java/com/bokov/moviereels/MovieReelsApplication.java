f2d9e08892f9e1cd31472a4073667c90bd7c9f7e
Uruskhan Bokov <bokovuruskhan@gmail.com>
2024-04-16 21:08:17 +0300

    spring initializr

---
+++ src/main/java/com/bokov/moviereels/MovieReelsApplication.java
@@ -0,0 +1,13 @@
+package com.bokov.moviereels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieReelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieReelsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bokov/moviereels/MovieReelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bokov.moviereels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieReelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
